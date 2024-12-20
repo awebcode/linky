7c2135c615efe201bae8a89866b83c0a2e7574e3
awebcode <asikurrahaman997@gmail.com>
2024-12-20 21:47:42 +0600

    updated

--- client/app/(auth)/reset-password/page.tsx
+++ client/app/(auth)/reset-password/page.tsx
@@ -1,13 +1,13 @@
 import ResetPassword from "@/modules/auth/reset-password";
 import { notFound } from "next/navigation";
 import React from "react";
+
 interface ResetProps {
-  searchParams: {
-    token: string;
-  };
+  searchParams: Promise<{ token: string }>;
 }
-const Reset: React.FC<ResetProps> = ({ searchParams: { token } }) => {
-    if(!token) return notFound()
+const Reset: React.FC<ResetProps> = async ({ searchParams }) => {
+  const { token } = await searchParams;
+  if (!token) return notFound();
   return (
     <>
       <ResetPassword token={token} />
